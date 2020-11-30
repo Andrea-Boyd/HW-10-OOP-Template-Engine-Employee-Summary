b4f53ee9b1414f590bbe9c862c97fa04d9808fc6
Andrea Boyd <andreanboyd@yahoo.com>
2020-11-30 14:17:04 -0600

    updated readme

--- Develop/app.js
+++ Develop/app.js
@@ -12,103 +12,112 @@ const render = require("./lib/htmlRenderer");
 
 let employees = []
 let roles = ["Manager", "Engineer", "Intern"]
-function getInfo(){
+function getInfo() {
 
     inquirer.prompt([
-        { 
+        {
             type: "input",
             name: "name",
-            message: "What is the new employee's name?"
+            message: "Enter the employee's name: "
         },
         {
             type: "input",
             name: "id",
-        message: "What is the new employee's id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the new employee's email?"
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "What role is the new Employee hired as?",
-        choices: roles
-    }
-]).then(function(basicInfo) {
-    if(basicInfo.role == "Manager"){
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "officeNum",
-                message: "Yo what's your office number man, bro?"
-            }
-        ]).then(function(managerInfo){
-            var manager = new Manager(basicInfo.name, basicInfo.id, basicInfo.email, managerInfo.officeNum);
-            employees.push(manager)
-            fs.writeFile(outputPath, render(employees), err => {if (err) throw err})
+            message: "Enter the employee's id: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the employee's email: "
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "Select the employee's role:",
+            choices: roles
+        }
+    ]).then(function (basicInfo) {
+        if (basicInfo.role == "Manager") {
             inquirer.prompt([
                 {
-                    type: "list",
-                    name: "next",
-                    message: "Do you have another employee to enter?",
-                    choices: ["yes", "no"]
+                    type: "input",
+                    name: "officeNum",
+                    message: "Enter the manager's office number: "
                 }
-            ]).then(function(confirmation){
-                if(confirmation.next == "yes"){
-                    getInfo()
-                }else{
-                    return
-                }
+            ]).then(function (managerInfo) {
+                var manager = new Manager(basicInfo.name, basicInfo.id, basicInfo.email, managerInfo.officeNum);
+                employees.push(manager)
+                fs.writeFile(outputPath, render(employees), err => { if (err) throw err })
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "next",
+                        message: "Do you need to enter information for another employee?",
+                        choices: ["yes", "no"]
+                    }
+                ]).then(function (confirmation) {
+                    if (confirmation.next == "yes") {
+                        getInfo()
+                    } else {
+                        return
+                    }
+                })
             })
-        })
-    }else if(basicInfo.role == "Engineer") {
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "github",
-                message: "What is the engineer's github?"
-            }
-        ]).then(function(engineerInfo){
-            console.log(basicInfo)
-            console.log(engineerInfo)
-        })
-    }else if(basicInfo.role == "Intern") {
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "school",
-                message: "What school are you attending?"
-            }
-        ]).then(function(internInfo){
-            console.log(basicInfo)
-            console.log(internInfo)
-        })
-    }
-    
-})
+        } else if (basicInfo.role == "Engineer") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "github",
+                    message: "Enter a link to the engineer's GitHub profile: "
+                }
+            ]).then(function (engineerInfo) {
+                var engineer = new Engineer(basicInfo.name, basicInfo.id, basicInfo.email, engineerInfo.github);
+                employees.push(engineer)
+                fs.writeFile(outputPath, render(employees), err => { if (err) throw err })
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "next",
+                        message: "Do you need to enter information for another employee?",
+                        choices: ["yes", "no"]
+                    }
+                ]).then(function (confirmation) {
+                    if (confirmation.next == "yes") {
+                        getInfo()
+                    } else {
+                        return
+                    }
+                })
+            })
+        } else if (basicInfo.role == "Intern") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "school",
+                    message: "Enter the school currently attended by the intern: "
+                }
+            ]).then(function (internInfo) {
+                var intern = new Intern(basicInfo.name, basicInfo.id, basicInfo.email, internInfo.school);
+                employees.push(intern)
+                fs.writeFile(outputPath, render(employees), err => { if (err) throw err })
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "next",
+                        message: "Do you need to enter information for another employee?",
+                        choices: ["yes", "no"]
+                    }
+                ]).then(function (confirmation) {
+                    if (confirmation.next == "yes") {
+                        getInfo()
+                    } else {
+                        return
+                    }
+                })
+            })
+        }
+
+    })
 }
-getInfo()
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+getInfo();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
